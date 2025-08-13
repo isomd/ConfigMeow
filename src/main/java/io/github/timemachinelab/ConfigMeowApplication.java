a401b0896937a84797d7a9e3716e2c73dd203479
lixiaochun <3250162035@qq.com>
2025-08-13 15:28:14 +0800

    init

---
+++ src/main/java/io/github/timemachinelab/ConfigMeowApplication.java
@@ -0,0 +1,13 @@
+package io.github.timemachinelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMeowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMeowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/timemachinelab/ConfigMeowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.timemachinelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMeowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
